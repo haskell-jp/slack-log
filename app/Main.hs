2ddcdda89dec633a0d2a12be830d184b26b5ab0f
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-08-01 08:57:34 +0900

    Fix: don't save channels.json when error
    Should not lose channel names by saveing an empty HashMap

--- app/Main.hs
+++ app/Main.hs
@@ -17,8 +17,8 @@ limitations under the License.
 {-|
   Export and upload messages from haskell-jp.slack.com
 -}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
 
 import           Control.Applicative      ((<|>))
 import           Control.Exception        (bracket)
@@ -43,8 +43,8 @@ import           System.IO                (BufferMode (NoBuffering), hGetEcho,
                                            hSetEcho, stderr, stdin, stdout)
 import qualified System.Process.Typed     as P
 import qualified Web.Slack                as Slack
-import qualified Web.Slack.Common         as Slack
 import qualified Web.Slack.Channel        as Slack
+import qualified Web.Slack.Common         as Slack
 
 import           Web.Slack.Instances      ()
 
@@ -93,16 +93,15 @@ main = do
 
   BL.writeFile ".timestamps.json" $ Json.encodePretty newTss
 
-  channelsByName <- Slack.channelsList (Slack.ListReq (Just True) (Just False))
+  Slack.channelsList (Slack.ListReq (Just True) (Just False))
     `runReaderT` config >>= \case
-      Right (Slack.ListRsp chs) -> return $! HM.fromList
-        $ map ((,) <$> Slack.channelId <*> Slack.channelName) chs
+      Right (Slack.ListRsp chs) -> do
+        let channelsByName = HM.fromList $ map ((,) <$> Slack.channelId <*> Slack.channelName) chs
+        BL.writeFile "doc/json/.channels.json" $ Json.encodePretty channelsByName
       Left err -> do
         hPutStrLn stderr $ "WARNING: Error when fetching the list of channels:"
         hPrint stderr err
-        return mempty
 
-  BL.writeFile "doc/json/.channels.json" $ Json.encodePretty channelsByName
 
   when (tss /= newTss) $ gitPushMessageLog
 
