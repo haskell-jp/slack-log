ab849ea586345327f8084662bbf63e33d6f2a99e
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-07-30 20:45:41 +0900

    Delete unused extensions

--- src/SlackLog/Html.hs
+++ src/SlackLog/Html.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
--- src/SlackLog/Types.hs
+++ src/SlackLog/Types.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StrictData        #-}
 
