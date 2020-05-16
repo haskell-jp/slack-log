c2a842d0ce771dde7abc13f5745cb6618a42b3f1
YAMAMOTO Yuji <whosekiteneverfly@gmail.com>
2020-05-16 19:16:00 +0900

    Pack into a single command with subcommands
    Fix https://github.com/haskell-jp/slack-log/issues/23

--- run.sample.sh
+++ run.sample.sh
@@ -5,7 +5,7 @@ set -eu
 cd "$(dirname "$0")"
 
 export SLACK_API_TOKEN='<ENTER_YOUR_LEGACY_API_TOKEN>'
-stack build --exec slack-log
+stack build --exec 'slack-log save'
 
 git add docs
 git commit -m"Slack log update at $(date)"
